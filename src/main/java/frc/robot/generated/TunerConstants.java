7f33405512a5fc9ae86a935b1b9ae309c5893089
dafestin79 <kumaro@ismanila.org>
2026-02-04 15:23:10 +0800

    Included comments for each offset

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -151,7 +151,7 @@ public class TunerConstants {
     private static final int kBackLeftDriveMotorId = 1;
     private static final int kBackLeftSteerMotorId = 2;
     private static final int kBackLeftEncoderId = 10;
-    private static final Angle kBackLeftEncoderOffset = Rotations.of(0.2259098); //-0.146728515625
+    private static final Angle kBackLeftEncoderOffset = Rotations.of(0.2259098); // -0.146728515625
     private static final boolean kBackLeftSteerMotorInverted = true;
     private static final boolean kBackLeftEncoderInverted = false;
 
@@ -162,7 +162,7 @@ public class TunerConstants {
     private static final int kBackRightDriveMotorId = 3;
     private static final int kBackRightSteerMotorId = 4;
     private static final int kBackRightEncoderId = 11;
-    private static final Angle kBackRightEncoderOffset = Rotations.of(-0.140625); //-0.140625
+    private static final Angle kBackRightEncoderOffset = Rotations.of(-0.140625); // -0.140625
     private static final boolean kBackRightSteerMotorInverted = true;
     private static final boolean kBackRightEncoderInverted = false;
 
