47e1ede627ab1268aa9d57fdea5c5897a3f2fbdf
dafestin79 <dafestin79@gmail.com>
2026-02-19 15:59:06 +0800

    Added base intake constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,31 @@
 package frc.robot;
 
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
 public final class Constants {
     public static class IntakeConstants {
-        
+        // Motor IDs
+        public static final int kRollerMotorId = 0; // Change to correct ID
+        public static final int kPivotMotorId = 0; // Change to correct ID
+
+        // General motor settings
+        public static final boolean kRollerInverted = false; // Change if motor is moving wrong way
+        public static final boolean kPivotInverted = false; // Change if motor is moving wrong way
+
+        public static final int kCurrentLimit = 40; // Adjust if needed
+        public static final IdleMode kIdleMode = IdleMode.kCoast; // Controls behavior when idle
+
+        // PID
+        public static final double kP = 0.0;
+        public static final double kI = 0.0;
+        public static final double kD = 0.0;
+
+        // Feedforward
+        public static final double kS = 0.0;
+        public static final double kV = 0.0;
+        public static final double kG = 0.0;
+        public static final double kA = 0.0;
+
+        public static final double TOLERANCE = 0.05; // Allowed error
     }
 }
