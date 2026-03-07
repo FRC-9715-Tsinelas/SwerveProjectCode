db25893f2176acc36cb2c8f77d4dc4d10fdffae3
dafestin79 <dafestin79@gmail.com>
2026-03-07 17:12:20 +0800

    Configured 3 motors for shooter subsystem

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,6 +7,7 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class ShooterSubsystem extends SubsystemBase {
     // one motor reverse of the other
@@ -19,58 +20,61 @@ public class ShooterSubsystem extends SubsystemBase {
      * to-do: add another motor config
      */
 
-    /*
-     * private final SparkMax motorLeader = new SparkMax(10, MotorType.kBrushless);
-     * private final SparkMax motorFollower = new SparkMax(3, MotorType.kBrushless);
-     * private final SparkMax smallRoller = new SparkMax(21, MotorType.kBrushless);
-     */
+    
+     private final SparkMax motorLeader = new SparkMax(10, MotorType.kBrushless);
+     private final SparkMax motorFollower = new SparkMax(3, MotorType.kBrushless);
+     private final SparkMax smallRoller = new SparkMax(21, MotorType.kBrushless);
+     
 
-    private final SparkMax leader = new SparkMax(10, MotorType.kBrushless); // CHANGE to correct
-    private final SparkMax follower = new SparkMax(3, MotorType.kBrushless); // CHANGE to correct
+    //private final SparkMax leader = new SparkMax(10, MotorType.kBrushless); // CHANGE to correct
+    //private final SparkMax follower = new SparkMax(3, MotorType.kBrushless); // CHANGE to correct
     // other can id is 21 i think
 
     private double currentPower = 0.0;
 
     public ShooterSubsystem() {
         SparkMaxConfig baseConfig = new SparkMaxConfig();
-        SparkMaxConfig leaderConfig = new SparkMaxConfig();
-        SparkMaxConfig followerConfig = new SparkMaxConfig();
+        // SparkMaxConfig leaderConfig = new SparkMaxConfig();
+        // SparkMaxConfig followerConfig = new SparkMaxConfig();
 
-        /*
-         * SparkMaxConfig motorFollowerConfig = new SparkMaxConfig();
-         * SparkMaxConfig smallRollerConfig = new SparkMaxConfig();
-         */
+        
+        SparkMaxConfig motorFollowerConfig = new SparkMaxConfig();
+        SparkMaxConfig smallRollerConfig = new SparkMaxConfig();
+         
 
         baseConfig
             .idleMode(IdleMode.kCoast)
-            .smartCurrentLimit(ShooterConstants.kLargeMotorCurrentLimit);
+            .smartCurrentLimit(ShooterConstants.kLargeMotorCurrentLimit)
+            .inverted(true);
 
-        /*
-         * motorLeader.configure(baseConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
-         * 
-         * motorFollowerConfig.apply(baseConfig)
-         *   .follow(motorLeader, false);
-         * motorFollower.configure(motorFollowerConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
-         * 
-         * smallRollerConfig
-         *    .idleMode(IdleMode.kCoast)
-         *    .smartCurrentLimit(ShooterConstants.kSmallMotorCurrentLimit)
-         * smallRoller.configure(baseConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
-         * 
-         */
+        
+        motorLeader.configure(baseConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
+         
+        motorFollowerConfig.apply(baseConfig)
+            .follow(motorLeader, true);
+        motorFollower.configure(motorFollowerConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
+        
+        smallRollerConfig
+            .idleMode(IdleMode.kCoast)
+            .smartCurrentLimit(ShooterConstants.kSmallMotorCurrentLimit)
+            .inverted(true);
 
-        leaderConfig.apply(baseConfig).inverted(true);
-        leader.configure(leaderConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
+            
+        smallRoller.configure(smallRollerConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
+        
 
-        followerConfig.apply(baseConfig).inverted(false).follow(leader, true);
-        follower.configure(followerConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
+        // leaderConfig.apply(baseConfig).inverted(true);
+        // leader.configure(leaderConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
+
+        // followerConfig.apply(baseConfig).inverted(false).follow(leader, false);
+        // follower.configure(followerConfig, com.revrobotics.ResetMode.kResetSafeParameters, com.revrobotics.PersistMode.kPersistParameters);
     }
     
     public void setShooterPower(double power) {
         currentPower = Math.max(0.0, Math.min(1.0, power));
-        leader.set(currentPower);
-        // motorLeader.set(currentPower);
-        // smallRoller.set(currentPower);
+        // leader.set(currentPower);
+        motorLeader.set(currentPower);
+        smallRoller.set(currentPower);
     }
 
     public double getCurrentPower() {
@@ -90,3 +94,4 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
 }
+
