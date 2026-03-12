2b9ecd5f3df5f2ec96e2f7d5f7e540eeb2978ad8
hmmmTree <118045182+hmmmTree@users.noreply.github.com>
2026-03-13 05:32:48 +0800

    Update and rename MagicFunction.java to SpecialFunction.java

--- src/main/java/frc/robot/subsystems/MagicFunction.java
+++
@@ -1,7 +0,0 @@
-package frc.robot;
-import frc.robot.Constants.botaimConstants;
-
-public void MagicFunction(double distance) {
-  power = botaimConstants.kMagicFunctionA * Math.pow(botaimConstants.kMagicFunctionB, distance)
-  return power
-}
---
+++ src/main/java/frc/robot/subsystems/SpecialFunction.java
@@ -0,0 +1,9 @@
+package frc.robot;
+import frc.robot.Constants.botaimConstants;
+
+public final class SpecialFunction {
+  public void MagicFunction(double distance) {
+    power = botaimConstants.kMagicFunctionA * Math.pow(botaimConstants.kMagicFunctionB, distance)
+    return power
+  }
+}
